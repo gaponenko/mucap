a9c5d355b84cd79aa5245414b4b8c1863c53b4cd
Andrei Gaponenko <gandr@fnal.gov>
2012-12-14 13:09:44 -0600

    Start mucap project code as a test release on top of Mu2e Offline.

---
+++ setup.sh
@@ -0,0 +1,7 @@
+# This should be sourced after setting up a base release.
+
+# Add the test release to PATH, LD_LIBRARY_PATH and PYTHON_PATH
+source bin/setup_mu2e_project.sh
+
+# Define the test release environment variables.
+source  ${MU2E_BASE_RELEASE}/bin/addlocal.sh
