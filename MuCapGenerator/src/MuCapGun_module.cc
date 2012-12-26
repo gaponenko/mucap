9c8cf5e503ae36e6675e1049a6460ed10b906c3c
Andrei Gaponenko <gandr@fnal.gov>
2012-12-25 22:50:42 -0600

    Optionally, histogram Ek and momentum in MuCapGun.

--- MuCapGenerator/src/MuCapGun_module.cc
+++ MuCapGenerator/src/MuCapGun_module.cc
@@ -21,6 +21,7 @@
 #include "art/Framework/Principal/Event.h"
 #include "art/Framework/Services/Registry/ServiceHandle.h"
 #include "art/Framework/Services/Optional/RandomNumberGenerator.h"
+#include "art/Framework/Services/Optional/TFileService.h"
 
 #include "ConditionsService/inc/GlobalConstantsHandle.hh"
 #include "ConditionsService/inc/ParticleDataTable.hh"
@@ -36,6 +37,8 @@
 #include "MuCapInterfaces/inc/ISpectrumGenerator.hh"
 
 #include "MuCapGenerator/inc/generatorHelpers.hh"
+
+#include "TH1.h"
 
 namespace mucap {
   using fhicl::ParameterSet;
@@ -54,6 +57,9 @@ namespace mucap {
     std::auto_ptr<IAngleGenerator> omega_;
     std::auto_ptr<ISpectrumGenerator> time_;
 
+    TH1* hek_;
+    TH1* hmomentum_;
+
   public:
     explicit MuCapGun(fhicl::ParameterSet const& pset);
     virtual void produce(art::Event& event);
@@ -67,15 +73,47 @@ namespace mucap {
     , ek_(makeSpectrumGenerator(pset.get<ParameterSet>("kineticEnergy"), eng_))
     , omega_(makeAngleGenerator(pset.get<ParameterSet>("angles"), eng_))
     , time_(makeSpectrumGenerator(pset.get<ParameterSet>("time"), eng_))
+    , hek_()
+    , hmomentum_()
   {
     produces<GenParticleCollection>();
+
+    ParameterSet hset(pset.get<ParameterSet>("histograms"));
+    art::ServiceHandle<art::TFileService> tfs;
+
+    {
+      ParameterSet hpars(hset.get<ParameterSet>("kineticEnergy", ParameterSet()));
+      if(!hpars.is_empty()) {
+        hek_ = tfs->make<TH1D>("kineticEnergy", "kinetic energy",
+                               hpars.get<unsigned>("nbins"),
+                               hpars.get<double>("xmin"),
+                               hpars.get<double>("xmax")
+                               );
+      }
+    }
+
+    {
+      ParameterSet hpars(hset.get<ParameterSet>("momentum", ParameterSet()));
+      if(!hpars.is_empty()) {
+        hmomentum_ = tfs->make<TH1D>("momentum", "momentum",
+                                     hpars.get<unsigned>("nbins"),
+                                     hpars.get<double>("xmin"),
+                                     hpars.get<double>("xmax")
+                                     );
+      }
+    }
+
   }
 
   void MuCapGun::produce(art::Event& event) {
     std::auto_ptr<GenParticleCollection> output(new GenParticleCollection);
 
-    const double etot = mass_ + ek_->generate();
+    const double ek = ek_->generate();
+    if(hek_) { hek_->Fill(ek); }
+
+    const double etot = mass_ + ek;
     const double pmag = sqrt(std::pow(etot,2) - std::pow(mass_, 2));
+    if(hmomentum_) { hmomentum_->Fill(pmag); }
 
     const CLHEP::HepLorentzVector momentum(pmag * omega_->generate(), etot);
 
