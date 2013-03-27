008814d1c6bb224a855d33aa719e8a86df6ef160
Andrei Gaponenko <gandr@fnal.gov>
2013-03-26 21:23:43 -0500

    Hardcode TWIST DC/PC plane numbering in Geometry service.

--- MuCapGeom/inc/Geometry.hh
+++ MuCapGeom/inc/Geometry.hh
@@ -3,6 +3,7 @@
 #ifndef MuCapGeom_inc_Geometry_hh
 #define MuCapGeom_inc_Geometry_hh
 
+#include <vector>
 #include "fhiclcpp/ParameterSet.h"
 #include "art/Framework/Services/Registry/ActivityRegistry.h"
 #include "art/Framework/Services/Registry/ServiceMacros.h"
@@ -10,6 +11,10 @@
 #include "CLHEP/Vector/ThreeVector.h"
 
 namespace mucap {
+
+  enum class WPType { PC, DC };
+  struct WPInfo { WPType wpt; unsigned int iplane; };
+
   class Geometry {
   public:
 
@@ -20,11 +25,16 @@ namespace mucap {
     const CLHEP::Hep3Vector& targetCenter() const { return targetCenter_; }
     double targetThickness() const { return targetThickness_; }
 
+    // globalPlaneNumber is 1-based
+    const WPInfo& wpByGlobalNumber(unsigned globalPlaneNumber) const { return wpi_.at(globalPlaneNumber-1); }
+
   private:
     fhicl::ParameterSet pset_;
 
     CLHEP::Hep3Vector targetCenter_;
     double targetThickness_;
+
+    std::vector<WPInfo> wpi_;
   };
 }
 
--- MuCapGeom/src/Geometry_service.cc
+++ MuCapGeom/src/Geometry_service.cc
@@ -19,6 +19,70 @@ namespace mucap {
     , targetThickness_(pset.get<ParameterSet>(TARGET_MODULE_TYPE)
                        .get<ParameterSet>("target")
                        .get<double>("thickness"))
+
+    , wpi_{
+      { WPType::PC, 1},
+      { WPType::PC, 2},
+      { WPType::PC, 3},
+      { WPType::PC, 4},
+
+      { WPType::DC, 1},
+      { WPType::DC, 2},
+      { WPType::DC, 3},
+      { WPType::DC, 4},
+      { WPType::DC, 5},
+      { WPType::DC, 6},
+      { WPType::DC, 7},
+      { WPType::DC, 8},
+      { WPType::DC, 9},
+      { WPType::DC, 10},
+      { WPType::DC, 11},
+      { WPType::DC, 12},
+      { WPType::DC, 13},
+      { WPType::DC, 14},
+      { WPType::DC, 15},
+      { WPType::DC, 16},
+      { WPType::DC, 17},
+      { WPType::DC, 18},
+      { WPType::DC, 19},
+      { WPType::DC, 20},
+      { WPType::DC, 21},
+      { WPType::DC, 22},
+
+      { WPType::PC, 5},
+      { WPType::PC, 6},
+      { WPType::PC, 7},
+      { WPType::PC, 8},
+
+      { WPType::DC, 23},
+      { WPType::DC, 24},
+      { WPType::DC, 25},
+      { WPType::DC, 26},
+      { WPType::DC, 27},
+      { WPType::DC, 28},
+      { WPType::DC, 29},
+      { WPType::DC, 30},
+      { WPType::DC, 31},
+      { WPType::DC, 32},
+      { WPType::DC, 33},
+      { WPType::DC, 34},
+      { WPType::DC, 35},
+      { WPType::DC, 36},
+      { WPType::DC, 37},
+      { WPType::DC, 38},
+      { WPType::DC, 39},
+      { WPType::DC, 40},
+      { WPType::DC, 41},
+      { WPType::DC, 42},
+      { WPType::DC, 43},
+      { WPType::DC, 44},
+
+      { WPType::PC, 9},
+      { WPType::PC, 10},
+      { WPType::PC, 11},
+      { WPType::PC, 12},
+    }
+
   {
     //----------------------------------------------------------------
     vector<ParameterSet> modulePars(pset_.get<vector<ParameterSet> >("chamberModules"));
