9d2e080e6f67b81274088804d01028cbc2afde28
Andrei Gaponenko <gandr@fnal.gov>
2013-11-28 17:21:04 -0800

    A helper script to run scans for the last plane distribution tuning.

---
+++ bin/stopdist.pl
@@ -0,0 +1,65 @@
+#!/usr/bin/perl -w
+#
+# Andrei Gaponenko, 2013
+
+use File::Basename;
+use File::Copy;
+use Getopt::Long;
+
+my $numEvents = 1000;
+my $g4OutFile = 'hist_g4beam.root';
+my $clark = '/home/andr/Clark/Clark';
+
+sub usage() {
+    return "
+Usage: stopdist.pl g4template.fcl clark_config {flat|gauss} momentum frac_width
+
+";
+}
+
+GetOptions('numEvents=i' => \$numEvents) or die "Bad command line\n";
+
+die usage() unless($#ARGV == 4);
+my ($template, $clarkconf, $shape, $momentum, $fwidth) = @ARGV;
+my $width = $fwidth * $momentum /2.;
+
+my $workdir = basename($template, ('.fcl')) . '_' . $shape . sprintf("_p%.3f", $momentum) . sprintf("_w%.5f", $fwidth);
+mkdir $workdir or die "Can't create directory $workdir: $!\n";
+
+
+#----------------------------------------------------------------
+# Prepare G4 inputs
+my $fcl = $workdir . '/' . basename($template);
+copy($template, $fcl) or die "Can't copy $template => $fcl: $!\n";
+
+open(my $fh, '>>', $fcl) or die "Can't append to $fcl: $!\n";
+print $fh "physics.producers.generate.energySpec.spectrum: $shape\n";
+if($shape eq "flat") {
+    print $fh "physics.producers.generate.energySpec.center: $momentum\n";
+    print $fh "physics.producers.generate.energySpec.halfWidth: $width\n";
+}
+elsif($shape eq 'gauss') {
+    print $fh "physics.producers.generate.energySpec.mean: $momentum\n";
+    print $fh "physics.producers.generate.energySpec.sigma: $width\n";
+}
+else {
+    die "Unknown spectrum $shape\n";
+}
+
+#----------------------------------------------------------------
+# Prepare Clark inputs
+
+copy($clarkconf, $workdir ) or die "Can't copy $clarkconf => $workdir: $!\n";
+
+#----------------------------------------------------------------
+# Run the jobs
+
+chdir $workdir or die "Can't cd to $workdir: $!\n";
+
+system("/usr/bin/time mu2e -n $numEvents -c " . basename($fcl) . " > mucap.log 2>&1") == 0
+    or die "Error running the G4 job\n";
+
+system("/usr/bin/time $clark -l clark.out -o clark.root ". basename($clarkconf) ." $g4OutFile > clark.log 2>&1") == 0
+    or die "Error running the Clark job\n";
+
+#----------------------------------------------------------------
